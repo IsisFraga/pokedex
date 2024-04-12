90f7a1df0266e47ea4453839184bbccd425e6f36
Isis Fraga <51866290+IsisFraga@users.noreply.github.com>
2024-04-12 12:53:30 -0300

    feat: initial configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,14 +9,34 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pokedex`,
+    description: `Gatsby Study`,
+    author: `Isis Fraga`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `Roboto`,
+            file: `https://fonts.googleapis.com/css2?family=Roboto&display=swap`,
+          },
+          {
+            name: `Preahvihear`,
+            file: `https://fonts.googleapis.com/css2?family=Preahvihear&display=swap`,
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
